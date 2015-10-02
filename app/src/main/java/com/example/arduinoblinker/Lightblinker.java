57d9b0e34b600d8c1fbdcf427326cb2f6297fe89
Neha Pathmanaban <neha.pathmanaban@gmail.com>
2015-10-02 13:28:45 -0400

    added calculator functionality

--- app/src/main/java/com/example/arduinoblinker/Lightblinker.java
+++ app/src/main/java/com/example/arduinoblinker/Lightblinker.java
@@ -9,6 +9,8 @@ import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.PendingIntent;
@@ -20,6 +22,8 @@ import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 import android.hardware.usb.UsbManager;
 import android.hardware.usb.UsbAccessory;
@@ -83,10 +87,18 @@ public class Lightblinker extends Activity {
                         getLastNonConfigurationInstance();
                 openAccessory(mAccessory);
             }
-            setContentView(R.layout.activity_lightblinker);
-            buttonLED = (ToggleButton) findViewById(R.id.toggleButtonLED);
-
+           // setContentView(R.layout.activity_lightblinker);
+           // buttonLED = (ToggleButton) findViewById(R.id.toggleButtonLED);
+            //calculator code
+            setContentView(R.layout.activity_main);
         }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
         @SuppressWarnings("deprecation")
         @Override
         public Object onRetainNonConfigurationInstance() {
@@ -154,18 +166,167 @@ public class Lightblinker extends Activity {
                 mAccessory = null;
             }
         }
-        public void blinkLED(View v){
-            byte[] buffer = new byte[1];
-            if(buttonLED.isChecked())
-                buffer[0]=(byte)0; // button says on, light is off
-            else
-                buffer[0]=(byte)1; // button says off, light is on
-            if (mOutputStream != null) {
-                try {
-                    mOutputStream.write(buffer);
-                } catch (IOException e) {
-                    Log.e(TAG, "write failed", e);
-                }
+       /* public void blinkLED(View v){
+                byte[] buffer = new byte[1];
+                if(buttonLED.isChecked())
+                    buffer[0]=(byte)0; // button says on, light is off
+                else
+                    buffer[0]=(byte)1; // button says off, light is on
+                if (mOutputStream != null) {
+                    try {
+                        mOutputStream.write(buffer);
+                    } catch (IOException e) {
+                        Log.e(TAG, "write failed", e);
+                    }
             }
+        }*/
+
+    ArrayList<String> arrayList = new ArrayList<String>();
+    String string = "";
+    String string1 = "";
+
+    public void onClick1 (View v){
+
+        TextView textView_commands = (TextView) findViewById(R.id.textView_commands);
+
+        Button button = (Button) v;
+
+        string = (String) button.getText().toString();
+
+        if(!string.contains("+") && !string.contains("-") && !string.contains("/") && !string.contains("*")) {
+            string1 = string1+string;
+
+            if (arrayList.size()>0) {
+
+                arrayList.remove((arrayList.size()-1));
+            }
+            arrayList.add(string1);
+
         }
+        else {
+
+            arrayList.add(string);
+            arrayList.add(string);
+            string1="";
+        }
+
+
+        textView_commands.setText(textView_commands.getText().toString() + string);
+//        textView_commands.setText(arrayList.toString());
+
+
     }
+
+    public void onClick (View v) {
+
+        TextView textView_results = (TextView)findViewById(R.id.textView_results);
+
+        int calc_result = 0;
+        int calc_size = arrayList.size();
+
+        while (calc_size!=1){
+            if (calc_size>3){
+
+                if (arrayList.get(3).contains("*") || arrayList.get(3).contains("/")){
+
+                    if (arrayList.get(3).contains("*")){
+                        calc_result = Integer.parseInt(arrayList.get(2))* Integer.parseInt(arrayList.get(4));
+                    }
+
+                    if (arrayList.get(3).contains("/")){
+                        calc_result = Integer.parseInt(arrayList.get(2))/ Integer.parseInt(arrayList.get(4));
+                    }
+
+                    arrayList.remove(2);
+                    arrayList.remove(2);
+                    arrayList.remove(2);
+                    arrayList.add(2, Integer.toString(calc_result));
+                    calc_size = arrayList.size();
+                }
+
+                else{
+
+                    if (arrayList.get(1).contains("+")){
+                        calc_result = Integer.parseInt(arrayList.get(0))+ Integer.parseInt(arrayList.get(2));
+                    }
+
+                    if (arrayList.get(1).contains("-")){
+                        calc_result = Integer.parseInt(arrayList.get(0))- Integer.parseInt(arrayList.get(2));
+                    }
+
+                    if (arrayList.get(1).contains("*")){
+                        calc_result = Integer.parseInt(arrayList.get(0))* Integer.parseInt(arrayList.get(2));
+                    }
+
+                    if (arrayList.get(1).contains("/")){
+                        calc_result = Integer.parseInt(arrayList.get(0))/ Integer.parseInt(arrayList.get(2));
+                    }
+
+                    arrayList.remove(0);
+                    arrayList.remove(0);
+                    arrayList.remove(0);
+                    arrayList.add(0, Integer.toString(calc_result));
+                    calc_size = arrayList.size();
+
+                }
+
+            }
+
+            else{
+                if (arrayList.get(1).contains("+")){
+                    calc_result = Integer.parseInt(arrayList.get(0))+ Integer.parseInt(arrayList.get(2));
+                }
+
+                if (arrayList.get(1).contains("-")){
+                    calc_result = Integer.parseInt(arrayList.get(0))- Integer.parseInt(arrayList.get(2));
+                }
+
+                if (arrayList.get(1).contains("*")){
+                    calc_result = Integer.parseInt(arrayList.get(0))* Integer.parseInt(arrayList.get(2));
+                }
+
+                if (arrayList.get(1).contains("/")){
+                    calc_result = Integer.parseInt(arrayList.get(0))/ Integer.parseInt(arrayList.get(2));
+                }
+
+                arrayList.remove(0);
+                arrayList.remove(0);
+                arrayList.remove(0);
+                arrayList.add(0, Integer.toString(calc_result));
+                calc_size = arrayList.size();
+            }
+
+        }
+
+        textView_results.setText(Integer.toString(calc_result));
+
+    }
+
+    public void clear (View v){
+        TextView textView_results = (TextView)findViewById(R.id.textView_results);
+        TextView textView_commands = (TextView)findViewById(R.id.textView_commands);
+
+        string1="";
+        string="";
+        textView_results.setText("0");
+        textView_commands.setText("");
+        arrayList.clear();
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
